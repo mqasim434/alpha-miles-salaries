ea48ccedc6cc54804012006d01a1f61d93fffe1c
mqasim434 <mq30003@gmail.com>
2024-07-12 12:12:51 +0500

    Built web
    Branch: main
    Commit: fe73f5a76b28710b72fd5642d45543eda29e1846 (dirty)
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5c3fe6d0ef6bd4203e3d80e6b33df411",
-"/": "5c3fe6d0ef6bd4203e3d80e6b33df411",
+"index.html": "2508c97c1b1f1014e318474d669131c7",
+"/": "2508c97c1b1f1014e318474d669131c7",
 "main.dart.js": "85d8b0322343ba914ddb4cc47f752058",
 "manifest.json": "389ade36e248848b817b6aa7ee04dbc1",
 "version.json": "de54dfc807c149b0fadc66c006d4e7b3"};
